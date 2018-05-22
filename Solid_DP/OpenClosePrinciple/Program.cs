0e56be93a37fc678fe7ecee8e3f74026b3ecbcfe
adrianB3 <balanescuadrian71@gmail.com>
2018-05-22 12:56:04 +0300

    Added Open-close principle

--- Solid_DP/OpenClosePrinciple/Program.cs
+++ Solid_DP/OpenClosePrinciple/Program.cs
@@ -1,12 +1,164 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace OpenClosePrinciple
 {
+    // Clases should be open for extension, closed for modification
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+    public enum Size
+    {
+        Small, Medium, Large, Huge
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public Color Color { get; set; }
+        public Size Size { get; set; }
+        public Product(string name, Color color, Size size)
+        {
+            Name = name ?? throw new ArgumentNullException(paramName: nameof(name));
+            Color = color;
+            Size = size;
+        }
+    }
+
+    public class ProductFilter
+    {
+        // The open-close principale is violated because, 
+        // for ex, if we want to extent add the capability to filter by size and color
+        // we would have to modify the class
+        // Solution is to use inheritance by implementing an 'enterprise pattern' called specification pattern
+        // implemented using a bunch of interfaces
+        public static IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var product in products)
+            {
+                if (product.Size == size)
+                {
+                    yield return product;
+                }
+            }
+        }
+
+        public static IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var product in products)
+            {
+                if (product.Color == color)
+                {
+                    yield return product;
+                }
+            }
+        }
+    }
+
+    public interface ISpecification<T>
+    {
+        bool IsSatisfied(T t);
+    }
+
+    public interface IFilter<T>
+    {
+        IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+    }
+
+    public class ColorSpecification : ISpecification<Product>
+    {
+        private Color color;
+
+        public ColorSpecification(Color color)
+        {
+            this.color = color;
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Color == color;
+        }
+    }
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private Size size;
+
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Size == size;
+        }
+    }
+
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first, second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first ?? throw new ArgumentNullException(paramName: nameof(first));
+            this.second = second ?? throw new ArgumentNullException(paramName: nameof(second));
+        }
+        public bool IsSatisfied(T t)
+        {
+            return first.IsSatisfied(t) && second.IsSatisfied(t);
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+        {
+            foreach (var item in items)
+            {
+                if (spec.IsSatisfied(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var apple = new Product("Apple",Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Huge);
+
+            Product[] products = {apple, tree, house};
+
+            Console.WriteLine("Green products (old method): ");
+            foreach (var p in ProductFilter.FilterByColor(products,Color.Green))
+            {
+                Console.WriteLine($"- {p.Name} is Green");
+            }
+
+            var bf = new BetterFilter();
+
+            Console.WriteLine("Green products (better filter): ");
+            foreach (var product in bf.Filter(products, new ColorSpecification(Color.Green)))
+            {
+                Console.WriteLine($"- {product.Name} is Green");
+            }
+
+            // Composite specification using a combinator
+            Console.WriteLine("Large blue items (better filter): ");
+            foreach (var p in bf.Filter(
+                products,
+                new AndSpecification<Product>(
+                    new ColorSpecification(Color.Blue),
+                    new SizeSpecification(Size.Huge)
+                )))
+            {
+                Console.WriteLine($"- {p.Name} is huge and blue");
+            }
         }
     }
 }
