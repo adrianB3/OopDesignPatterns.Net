c7ca09ee2caab95f78fac300d86f79eb5ae74751
adrianB3 <balanescuadrian71@gmail.com>
2018-05-25 16:15:40 +0300

    Added builder

---
+++ Builder_DP/Builder_DP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace WithoutBuilder
+{
+    class Program
+    {
+        // Little example to show how a program that is supposed to
+        // output html format content without using a builder
+        // The program easily gets very clutered adding more functionality
+        static void Main(string[] args)
+        {
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            Console.WriteLine(sb);
+
+            var words = new[] {"hello", "world"};
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (var word in words)
+            {
+                sb.AppendFormat("<li>{0}</li>", word);
+            }
+
+            sb.Append("</ul>");
+            Console.WriteLine(sb);
+        }
+    }
+}
---
+++ Builder_DP/HTML_Builder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HTML_Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
