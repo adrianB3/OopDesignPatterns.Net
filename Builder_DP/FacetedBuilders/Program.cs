7bb6d862f9c6bd285fb8b03290c170614ef14816
adrianB3 <balanescuadrian71@gmail.com>
2018-05-27 18:04:20 +0300

    Added faceted builder

---
+++ Builder_DP/FacetedBuilders/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace FacetedBuilders
+{
+    // For constructing big objects we need multiple builders
+    // that will handle different parts of the initialization
+    // This will be accomplished using a builder facet
+    public class Person
+    {
+        // address
+        public string StreetAddress, Postcode, City;
+
+        // employment
+        public string CompanyName, Postion;
+        public int AnnualIncome;
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode}, {nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}, {nameof(Postion)}: {Postion}, {nameof(AnnualIncome)}: {AnnualIncome}";
+        }
+    }
+
+    public class PersonBuilder  // facade, not an actual builder
+    {
+        // as a reference!
+        protected Person person = new Person();
+
+        public PersonJobBuilder Works => new PersonJobBuilder(person);
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+
+        public static implicit operator Person(PersonBuilder pb)
+        {
+            return pb.person;
+        }
+    }
+
+    public class PersonJobBuilder : PersonBuilder
+    {
+        // used for building informations about the job for a person
+        public PersonJobBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonJobBuilder At(string companyName)
+        {
+            person.CompanyName = companyName;
+            return this;
+        }
+
+        public PersonJobBuilder AsA(string position)
+        {
+            person.Postion = position;
+            return this;
+        }
+
+        public PersonJobBuilder Earning(int amount)
+        {
+            person.AnnualIncome = amount;
+            return this;    
+        }
+    }
+
+    public class PersonAddressBuilder : PersonBuilder
+    {
+        public PersonAddressBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonAddressBuilder At(string streetAddress)
+        {
+            person.StreetAddress = streetAddress;
+            return this;
+        }
+
+        public PersonAddressBuilder WithPostcode(string postcode)
+        {
+            person.Postcode = postcode;
+            return this;
+        }
+
+        public PersonAddressBuilder In(string city)
+        {
+            person.City = city;
+            return this;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pb = new PersonBuilder();
+            Person person = pb
+                .Lives.At("24 Cornelia Salceanu")
+                    .In("Timisoara")
+                    .WithPostcode("TM123")
+                .Works.At("CodeWorks")
+                    .AsA("Software Engineer")
+                    .Earning(95000);
+            Console.WriteLine(person);
+        }
+    }
+}
