c14b891003eff1e81d30e0109e19da82f99ebd84
adrianB3 <balanescuadrian71@gmail.com>
2018-05-27 16:36:23 +0300

    Added Html builder

--- Builder_DP/HTML_Builder/Program.cs
+++ Builder_DP/HTML_Builder/Program.cs
@@ -1,12 +1,111 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace HTML_Builder
 {
+    class HtmlElement
+    {
+        public string Name, Text;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+        private const int IndentSize = 2;
+
+        public HtmlElement()
+        {
+            
+        }
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Text = text ?? throw new ArgumentNullException(nameof(text));
+        }
+
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', IndentSize * indent);
+            sb.AppendLine($"{i}<{Name}>");
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', IndentSize * (indent + 1)));
+                sb.AppendLine(Text);
+            }
+
+            foreach (var htmlElement in Elements)
+            {
+                sb.Append(htmlElement.ToStringImpl(indent + 1));
+            }
+            sb.AppendLine($"{i}</{Name}>");
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(0);
+        }
+    }
+
+    public class HtmlBuilder
+    {
+        private readonly string rootName; 
+        // statefull builder
+        HtmlElement root = new HtmlElement();
+
+        public HtmlBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.Name = rootName;
+        }
+
+        public HtmlBuilder AddChild(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            root.Elements.Add(e);
+            return this; // Fluent Builder -- now the builder it's able to chain AddChild methods
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement{Name = rootName};
+        }
+    }
+
     class Program
     {
+        // A HTML builder
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // From WithoutBuilder
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.Append("</p>");
+            Console.WriteLine(sb);
+
+            var words = new[] { "hello", "world" };
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (var word in words)
+            {
+                sb.AppendFormat("<li>{0}</li>", word);
+            }
+
+            sb.Append("</ul>");
+            Console.WriteLine(sb);
+
+            // With builder
+
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li","Hello").AddChild("li","World"); // Fluent Interface
+            Console.WriteLine(builder);
         }
     }
 }
