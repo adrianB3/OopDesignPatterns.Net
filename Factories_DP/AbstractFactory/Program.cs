fdbf2a7af5d2bd5d76d879dbb54aef3be0b3a5c5
adrianB3 <balanescuadrian71@gmail.com>
2018-05-28 17:46:28 +0300

    Abstract factory (not implemented)

---
+++ Factories_DP/AbstractFactory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Implement an Abstract Factory that is not violating the Open-close principle
+        }
+    }
+}
