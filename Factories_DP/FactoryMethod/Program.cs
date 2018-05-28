ac2254cc41fac2d23e49313462b9e8ce3297d7b8
adrianB3 <balanescuadrian71@gmail.com>
2018-05-28 16:29:23 +0300

    Added factory method

---
+++ Factories_DP/FactoryMethod/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    public class Point
+    {
+        // Points are initialized through Factory Methods
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho*Math.Cos(theta), rho*Math.Sin(theta));
+        }
+
+        private double x, y;
+
+        private Point(double x, double y) // the constructor becomes private
+        {
+            this.x = x;
+            this.y = y;
+        }
+        
+        // what if we want a constructor for polar coordinates?
+        // the constructor cannot be overloaded as the signature would be the same 
+        // public Point(double rho, double theta)
+        // A solution to this is using Factories
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = Point.NewPolarPoint(1.0, Math.PI/2);
+            Console.WriteLine(point);
+        }
+    }
+}
