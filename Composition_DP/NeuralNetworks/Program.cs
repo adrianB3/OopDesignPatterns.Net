a87b2bc924ff152cb429aa315a29859cd270190d
adrianB3 <balanescuadrian71@gmail.com>
2018-06-04 19:00:34 +0300

    Added Composition DP

---
+++ Composition_DP/NeuralNetworks/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace NeuralNetworks
+{
+    public static class ExtentionMethods
+    {
+        public static void ConnectTo(this IEnumerable<Neuron> self, IEnumerable<Neuron> other)
+        {
+            if(ReferenceEquals(self, other)) return;
+
+            foreach (var From in self)
+            foreach (var To in other)
+            {
+                From.Out.Add(To);
+                To.In.Add(From);
+            }
+        }
+    }
+
+    public class Neuron : IEnumerable<Neuron>
+    {
+        public float Value;
+        public List<Neuron> In, Out;
+
+        public IEnumerator<Neuron> GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class NeuronLayer : Collection<Neuron>
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var neuron1 = new Neuron();
+            var neuron2 = new Neuron();
+            
+            neuron1.ConnectTo(neuron2);
+
+            var neuronLayer1 = new NeuronLayer();
+            var neuronLayer2 = new NeuronLayer();
+
+            neuron1.ConnectTo(neuronLayer1);
+            neuronLayer2.ConnectTo(neuronLayer1);
+        }
+    }
+}
---
+++ Composition_DP/Shapes/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    // Treating complex composite entities as simple ones
+    // the same as grouping shapes and moving them as a single shape
+
+    public class GraphicObject
+    {
+        public string Color;
+        public virtual string Name { get; set; } = "Group";
+
+        private Lazy<List<GraphicObject>> children = new Lazy<List<GraphicObject>>(); // the list is instantiated only if there is a need for it
+        public List<GraphicObject> Children => children.Value;
+
+        public void Print(StringBuilder sb, int depth)
+        {
+            sb.Append(new string('*', depth))
+                .Append(string.IsNullOrWhiteSpace(Color) ? string.Empty : $"{Color} ")
+                .AppendLine(Name);
+
+            foreach (var child in Children)
+            {
+                child.Print(sb, depth+1);
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            Print(sb, 0);
+            return sb.ToString();
+        }
+    }
+
+    public class Circle : GraphicObject
+    {
+        public override string Name => "Circle";
+    }
+
+    public class Square : GraphicObject
+    {
+        public override string Name => "Square";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var drawing = new GraphicObject
+                {Name = "My Drawing"};
+            drawing.Children.Add(new Square {Color = "Red"});
+            drawing.Children.Add(new Circle {Color = "Blue"});
+
+            var group = new GraphicObject();
+            group.Children.Add(new Circle {Color = "Yellow"});
+            group.Children.Add(new Square {Color = "Yellow"});
+
+            drawing.Children.Add(group);
+
+            Console.WriteLine(drawing);
+        }
+    }
+}
