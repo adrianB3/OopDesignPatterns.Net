b5f20b3e86dd18334c00f0175a45761899f855e4
Bălănescu Adrian <balanescuadrian71@gmail.com>
2018-06-09 20:06:17 +0300

    Added Observer pattern

---
+++ Observer_DP/Observer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Observer
+{
+    /// <summary>
+    ///
+    /// An Observer is an object that wishes
+    /// to be informed about events happening
+    /// in the system.
+    /// The entity generating the event
+    /// is an observable.
+    /// 
+    /// </summary>
+
+    public class FallsIllEventArgs
+    {
+        public string Address;
+    }
+
+    public class Person
+    {
+        public void CatchCold()
+        {
+            FallsIll?.Invoke(this, new FallsIllEventArgs {Address = "123 Sesame street"});
+        }
+        // The Observer pattern is basicly baked in 
+        // the .Net infrastructure using the ideea of events
+        public event EventHandler<FallsIllEventArgs> FallsIll; 
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.FallsIll += CallDoctor; // CallDoctor Method is subscribeing to the event
+            person.CatchCold();
+            person.FallsIll -= CallDoctor;
+        }
+
+        private static void CallDoctor(object sender, FallsIllEventArgs e)
+        {
+            Console.WriteLine($"A doctor has been called at address: {e.Address}");
+        }
+    }
+}
---
+++ Observer_DP/ReactiveExtensions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ReactiveExtensions.Annotations;
+
+namespace ReactiveExtensions
+{
+    public class Market
+    {
+        public BindingList<float> Prices = new BindingList<float>();
+        public void AddPrice(float price)
+        {
+            Prices.Add(price);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var market = new Market();
+            market.Prices.ListChanged += (sender, eventArgs) =>
+            {
+                if (eventArgs.ListChangedType == ListChangedType.ItemAdded)
+                {
+                    float price = ((BindingList<float>) sender)[eventArgs.NewIndex];
+                    Console.WriteLine($"Binding list got a price of {price}");
+                }
+            };
+            market.AddPrice(123f);
+        }
+    }
+}
