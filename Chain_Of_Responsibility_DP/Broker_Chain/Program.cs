cdce3a3647ad13853eccff5caa0e7cc51653b820
Bălănescu Adrian <balanescuadrian71@gmail.com>
2018-06-12 19:18:38 +0300

    Added Chain_Of_Resp_DP

---
+++ Chain_Of_Responsibility_DP/Broker_Chain/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+namespace Broker_Chain
+{
+    class Program
+    {
+        public class Game // the mediator
+        {
+            public event EventHandler<Query> Queries;
+
+            public void PerformQuery(object sender, Query q)
+            {
+                Queries?.Invoke(sender, q);
+            }
+        }
+
+        public class Query
+        {
+            public string CreatureName;
+            public enum Argument
+            {
+                Attack, Defense
+            }
+
+            public Argument WhatToQuery;
+            public int Value;
+
+            public Query(string creatureName, Argument whatToQuery, int value)
+            {
+                CreatureName = creatureName ?? throw new ArgumentNullException(nameof(creatureName));
+                WhatToQuery = whatToQuery;
+                Value = value;
+            }
+        }
+
+        public class Creature
+        {
+            public Game game;
+            public string Name;
+            private int attack, defense;
+
+            public Creature(Game game, string name, int attack, int defense)
+            {
+                this.game = game ?? throw new ArgumentNullException(nameof(game));
+                Name = name ?? throw new ArgumentNullException(nameof(name));
+                this.attack = attack;
+                this.defense = defense;
+            }
+
+            public int Attack
+            {
+                get
+                {
+                    // collect Bonuses
+                    var q = new Query(Name, Query.Argument.Attack, attack);
+                    game.PerformQuery(this, q); // q.Value -> actual attack value
+                    return q.Value;
+                }
+            }
+
+            public int Defense
+            {
+                get
+                {
+                    // collect Bonuses
+                    var q = new Query(Name, Query.Argument.Defense, attack);
+                    game.PerformQuery(this, q); // q.Value -> actual defense value
+                    return q.Value;
+                }
+            }
+
+            public override string ToString()
+            {
+                return $"{nameof(Name)}: {Name}, {nameof(Attack)}: {Attack}, {nameof(Defense)}: {Defense}";
+            }
+        }
+
+        public abstract class CreatureModifier : IDisposable
+        {
+            protected Game game;
+            protected Creature creature;
+
+            protected CreatureModifier(Game game, Creature creature)
+            {
+                this.game = game ?? throw new ArgumentNullException(nameof(game));
+                this.creature = creature ?? throw new ArgumentNullException(nameof(creature));
+                game.Queries += Handle; // subscribe to smth abstract so that the derived classes should be called
+            }
+
+            protected abstract void Handle(object sender, Query q);
+
+            public void Dispose()
+            {
+                game.Queries -= Handle; // unsubscribe
+            }
+        }
+
+        public class DoubleAttackModifier : CreatureModifier
+        {
+            public DoubleAttackModifier(Game game, Creature creature) : base(game, creature)
+            {
+            }
+
+            protected override void Handle(object sender, Query q)
+            {
+                if (q.CreatureName == creature.Name && q.WhatToQuery == Query.Argument.Attack)
+                {
+                    q.Value *= 2;
+                }
+            }
+        }
+
+        public class IncreaseDefenseModifier : CreatureModifier
+        {
+            public IncreaseDefenseModifier(Game game, Creature creature) : base(game, creature)
+            {
+            }
+
+            protected override void Handle(object sender, Query q)
+            {
+                if (q.CreatureName == creature.Name && q.WhatToQuery == Query.Argument.Defense)
+                {
+                    q.Value++;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var game = new Game();
+            var goblin = new Creature(game,"Gobin", 2,2);
+            Console.WriteLine(goblin);
+
+            using (new DoubleAttackModifier(game, goblin))
+            {
+                Console.WriteLine(goblin);
+                using (new IncreaseDefenseModifier(game, goblin))
+                {
+                    Console.WriteLine(goblin);
+                }
+            }
+
+            Console.WriteLine(goblin); // we can now go back to previous state
+        }
+    }
+
+    
+}
---
+++ Chain_Of_Responsibility_DP/Method_Chain/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Method_Chain
+{
+    public class Creature
+    {
+        public string Name;
+        public int Attack, Defense;
+
+        public Creature(string name, int attack, int defense)
+        {
+            Name = name;
+            Attack = attack;
+            Defense = defense;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Attack)}: {Attack}, {nameof(Defense)}: {Defense}";
+        }
+    }
+
+    public class CreatureModifier
+    {
+        protected Creature creature;
+        protected CreatureModifier next; // a linked list actually
+
+        public CreatureModifier(Creature creature)
+        {
+            this.creature = creature ?? throw new ArgumentNullException(nameof(creature));
+        }
+
+        public void Add(CreatureModifier cm)
+        {
+            if (next != null)
+            {
+                next.Add(cm);
+            }
+            else
+            {
+                next = cm;
+            }
+        }
+
+        public virtual void Handle() => next?.Handle();
+    }
+
+    public class DoubleAttackModifier : CreatureModifier
+    {
+        public DoubleAttackModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            Console.WriteLine($"Doubling {creature.Name}'s attack.");
+            creature.Attack *= 2;
+            base.Handle();
+        }
+    }
+
+    public class IncreaseDefenseModifier : CreatureModifier
+    {
+        public IncreaseDefenseModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            Console.WriteLine($"Increasing {creature.Name}'s defense.");
+            creature.Defense++;
+            base.Handle();
+        }
+    }
+
+    public class NoBonusesModifier : CreatureModifier
+    {
+        public NoBonusesModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            // do nothing so that the next modifiers will not be called
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var goblin = new Creature("Goblin", 2,2);
+            Console.WriteLine(goblin);
+
+            var root = new CreatureModifier(goblin);
+
+            //root.Add(new NoBonusesModifier(goblin));
+
+            root.Add(new DoubleAttackModifier(goblin));
+            root.Add(new IncreaseDefenseModifier(goblin));
+
+            root.Handle(); // takes care of every modifier in the chain
+
+            Console.WriteLine(goblin);
+        }
+    }
+}
