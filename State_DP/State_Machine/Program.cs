15c00fa0aedfe503b975b0a079ab7a324c716b4a
Bălănescu Adrian <balanescuadrian71@gmail.com>
2018-06-14 08:55:43 +0300

    Added State Machine

---
+++ State_DP/State_Machine/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace State_Machine
+{
+    public enum State
+    {
+        OffHook,
+        Connecting,
+        Connected,
+        OnHold
+    }
+
+    public enum Trigger
+    {
+        CallDialed,
+        HungUp,
+        CallConnected,
+        PlaceOnHold,
+        TakenOffHold,
+        LeftMessage
+    }
+
+    class Program
+    {
+        private static Dictionary<State, List<(Trigger, State)>> rules = new Dictionary<State, List<(Trigger, State)>>
+        {
+            [State.OffHook] = new List<(Trigger, State)>
+            {
+                (Trigger.CallDialed, State.Connecting)
+            },
+            [State.Connecting] = new List<(Trigger, State)>
+            {
+                (Trigger.HungUp, State.OffHook),
+                (Trigger.CallConnected, State.Connected)
+            },
+            [State.Connected] = new List<(Trigger, State)>
+            {
+                (Trigger.LeftMessage, State.OffHook),
+                (Trigger.HungUp, State.OffHook),
+                (Trigger.PlaceOnHold, State.OnHold)
+            },
+            [State.OnHold] = new List<(Trigger, State)>
+            {
+                (Trigger.TakenOffHold, State.Connected),
+                (Trigger.HungUp, State.OffHook)
+            }
+        };
+        static void Main(string[] args)
+        {
+            var state = State.OffHook;
+            while (true)
+            {
+                Console.WriteLine($"The phone is currently {state}");
+                Console.WriteLine("Select a trigger: ");
+                for (var i = 0; i < rules[state].Count; i++)
+                {
+                    var (t, _) = rules[state][i];
+                    Console.WriteLine($"{i}. {t}");
+                }
+
+                int input = int.Parse(Console.ReadLine());
+                var (_, s) = rules[state][input];
+                state = s;                
+            }
+        }
+    }
+}
